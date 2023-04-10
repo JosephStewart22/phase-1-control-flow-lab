526e2afb91fb16b0e1abe8d02e1236e3e73d7134
Joseph Stewart <stewartjoseph255@gmail.com>
2023-04-10 17:48:54 -0600

    Control Flow Lab

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400) {
+    return ("This one is on me!");
+  }
+  else if (someValue <= 2000){
+    return ("That will be twenty bucks.");
+  }
+  else if (someValue <= 2500) {
+    return ("I will gladly take your thirty bucks.")
+  }
+  else if (someValue >= 2500) {
+    return ("No can do.")
+  }
+  }
+
+function ternaryCheckCity(someCity){
+  return someCity === 'NYC' ? "Ok, sounds good." : "No go."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+const tip = 'tipValue';
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+switch (tip) {
+  case "generous":
+      return ("Thank you so much.");
+      break;
+  case "not as generous":
+      return ("Thank you.")
+      break;
+default: 
+  return("Bye.");
+}
 }
